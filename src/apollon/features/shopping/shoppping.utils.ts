5c8aa08e7644526590c0d6fcd2f4d976de8723b0
pixiedie <emilie.luzi@masteos.com>
2022-06-29 11:16:02 +0300

    reinit

--- src/apollon/features/shopping/shoppping.utils.ts
+++
@@ -1,8 +0,0 @@
-import { BackendTaskType } from './hooks/useShopping';
-import { TaskTypeTaskListType } from '../../../aphrodite/organisms/task-list/TaskList';
-
-type FormatTaskType = (tasks: BackendTaskType[]) => TaskTypeTaskListType;
-export const formatTask: FormatTaskType = tasks => tasks.map(({ name, ...rest }) => ({
-  ...rest,
-  label: name,
-}));
